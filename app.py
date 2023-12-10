e7b1d8a810e7f7285e4b86a0e2bbe397344c2101
michal <namoopsoo>
2023-12-10 17:06:54 -0500

    ok

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ DATA_DIR = "."
 HF_TOKEN = os.getenv("HF_TOKEN")
 LIVE_APP = os.getenv("LIVE_APP", "no")
 
-st.title("Look at this restaurant data from Kaggle🪿")
 
 menusdf = pd.read_csv(Path(DATA_DIR) / "restaurant-menus.csv")
 menusdf = menusdf[menusdf["description"].notnull()].copy()
@@ -25,8 +24,6 @@ restaurantsdf = pd.read_csv(Path(DATA_DIR) / "restaurants.csv")
 restaurant_vec = restaurantsdf.to_dict(orient="records")
 restaurant_map = {x["id"]: {"name": x["name"], "full_address": x["full_address"]} for x in restaurant_vec}
 
-st.write(menusdf.head())
-st.write("mmkay")
 
 
 model_name = "all-MiniLM-L12-v2"
@@ -37,6 +34,11 @@ embedder = SentenceTransformer(
 # embedder = SentenceTransformer(model_name)
 
 sampledf = menusdf.sample(n=1000).reset_index()
+
+st.title("Look at this restaurant data from Kaggle🪿")
+st.write(sampledf.head())
+st.write("mmkay")
+
 restaurant_id_map = {i: x for i, x in enumerate(sampledf["restaurant_id"].tolist())}
 # list(restaurant_id_map.items())[:5]
 sentences_1000 = sampledf["concat"].tolist()
@@ -86,51 +88,47 @@ def do_search(query):
 
 
 ########## ########## ########## ########## ########## ##########
-"""
-st.title("Let's look at the distribution of word counts for these columns")
-
-fig, axes = plt.subplots(figsize=(12,6), nrows=3, ncols=1)
-fig.patch.set_facecolor("xkcd:mint green")
-plt.tight_layout()
-for i, col in enumerate(["category", "name", "description"]):
-
-    menusdf[col + "_num_tokens"] = menusdf[col].map(lambda x: len(x.split(" "))) #  if isinstance(x, str) else 0
-    ax = axes[i] #fig.add_subplot(int(f"31{i + 1}"))
-    
-    ax.hist(menusdf[col + "_num_tokens"], bins=50)
-    ax.set(title=f"{col} num tokens")
-
-st.pyplot(fig)
-"""
+#st.title("Let's look at the distribution of word counts for these columns")
+#
+#fig, axes = plt.subplots(figsize=(12,6), nrows=3, ncols=1)
+#fig.patch.set_facecolor("xkcd:mint green")
+#plt.tight_layout()
+#for i, col in enumerate(["category", "name", "description"]):
+#
+#    menusdf[col + "_num_tokens"] = menusdf[col].map(lambda x: len(x.split(" "))) #  if isinstance(x, str) else 0
+#    ax = axes[i] #fig.add_subplot(int(f"31{i + 1}"))
+#    
+#    ax.hist(menusdf[col + "_num_tokens"], bins=50)
+#    ax.set(title=f"{col} num tokens")
+#
+#st.pyplot(fig)
 
 ########## ########## ########## ########## ########## ##########
-"""
-st.title("Paraphrase mining")
-st.title("Lets run paraphrase mining on a 1000 row sample of this menu data")
-st.write("First five sentences,")
-st.write(sentences_1000[:5])
-
-paraphrases = util.paraphrase_mining(embedder, sentences_1000)
-
-
-# paraphrases_with_restaurant_ids = [x for x in paraphrases ]
-for paraphrase in [row for row in paraphrases 
-                   if (row[0] < .99
-                   and (restaurant_id_map[row[1]] != restaurant_id_map[row[2]])) 
-                  ][:5]:
-    score, i, j = paraphrase
-    restaurant_id_1, restaurant_id_2 = (restaurant_id_map[i], restaurant_id_map[j])
-    st.write(
-        f"{sentences_1000[i]} (restaurant={restaurant_id_1})\n{sentences_1000[j]} (restaurant={restaurant_id_2})\n Score: {score:.4f}\n\n" 
-         )
-"""
+#st.title("Paraphrase mining")
+#st.title("Lets run paraphrase mining on a 1000 row sample of this menu data")
+#st.write("First five sentences,")
+#st.write(sentences_1000[:5])
+#
+#paraphrases = util.paraphrase_mining(embedder, sentences_1000)
+#
+#
+## paraphrases_with_restaurant_ids = [x for x in paraphrases ]
+#for paraphrase in [row for row in paraphrases 
+#                   if (row[0] < .99
+#                   and (restaurant_id_map[row[1]] != restaurant_id_map[row[2]])) 
+#                  ][:5]:
+#    score, i, j = paraphrase
+#    restaurant_id_1, restaurant_id_2 = (restaurant_id_map[i], restaurant_id_map[j])
+#    st.write(
+#        f"{sentences_1000[i]} (restaurant={restaurant_id_1})\n{sentences_1000[j]} (restaurant={restaurant_id_2})\n Score: {score:.4f}\n\n" 
+#         )
 
 
 ########## ########## ########## ########## ########## ##########
 st.title("Use cosine simularity for a phrase")
 # st.session_state.query
 query = ""
-query = st.text_area("Input to search for.", on_click=do_search, args=(query, ))
+query = st.text_area("Input to search for.", on_change=do_search, args=(query, ))
 # button_ok = st.button("Search")
 
 st.write("Ok bye.")
